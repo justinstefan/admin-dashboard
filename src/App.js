2c344eeba5851a3419c3f4c27e30e3f06c227fe0
Marius-Adam <marius.c.adam@gmail.com>
2020-05-12 20:06:57 +0100

    Added SubNavbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.scss";
 
 import Navbar from "./components/Navbar";
+import SubNavbar from "./components/SubNavbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SubNavbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
@@ -15,20 +14,16 @@ export default function Navbar() {
         <Toolbar>
           <div className="Logo">
             <a href="/">
-              <img src={Logo} href="/" alt="site logo" height="30" width="80" />
+              <img src={Logo} href="/" alt="site logo" height="30" width="84" />
             </a>
           </div>
           <div style={{ flexGrow: "1" }} />
           <div>
-            <IconButton aria-label="show 4 new mails" color="primary">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
+            <IconButton color="primary">
+              <MailIcon />
             </IconButton>
-            <IconButton aria-label="show 17 new notifications" color="primary">
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon />
-              </Badge>
+            <IconButton color="primary">
+              <NotificationsIcon />
             </IconButton>
             <IconButton
               edge="end"
